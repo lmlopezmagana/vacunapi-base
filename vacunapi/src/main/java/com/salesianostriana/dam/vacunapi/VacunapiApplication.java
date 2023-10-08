b9f8e0d4cb3c28be4b2eac5fedc6bc7c4625375b
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2023-10-08 18:04:04 +0200

    proyecto generado

---
+++ vacunapi/src/main/java/com/salesianostriana/dam/vacunapi/VacunapiApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.vacunapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacunapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacunapiApplication.class, args);
+	}
+
+}
---
+++ vacunapi/src/test/java/com/salesianostriana/dam/vacunapi/VacunapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.vacunapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacunapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
